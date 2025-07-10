bae47b8aed5479b2164582ae7f929efab618ae59
iamfrdfa <fri.faraji@gmail.com>
2025-07-10 12:29:08 +0200

    added contact form httpclient

--- portfolio/src/app/contactform/contactform.component.ts
+++ portfolio/src/app/contactform/contactform.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {Component, inject} from '@angular/core';
+import {FormsModule, NgForm} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
     selector: 'app-contactform',
@@ -8,13 +9,46 @@ import {FormsModule} from '@angular/forms';
     styleUrl: './contactform.component.scss'
 })
 export class ContactformComponent {
+    http = inject(HttpClient);
 
+    // The contact form component
+    // contactData is the model for the form
     contactData = {
         name: "",
         email: "",
         message: ""
     }
-    onSubmit() {
-        console.log(this.contactData);
+
+    mailTest = true;
+
+    post = {
+        endPoint: 'https://deineDomain.de/sendMail.php',
+        body: (payload: any) => JSON.stringify(payload),
+        options: {
+            headers: {
+                'Content-Type': 'text/plain',
+                responseType: 'text',
+            },
+        },
+    };
+
+    onSubmit(ngForm: NgForm) {
+        if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+            this.http
+                .post(this.post.endPoint, this.post.body(this.contactData))
+                .subscribe({
+                    next: (response) => {
+
+                        ngForm.resetForm();
+                    },
+                    error: (error) => {
+                        console.error(error);
+                    },
+                    complete: () => console.info('send post complete'),
+                });
+        } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+            ngForm.resetForm();
+        }
     }
 }
