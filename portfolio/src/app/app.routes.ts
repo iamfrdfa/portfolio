06ba9a22c3b702f4e312aae96b453c75fbdaa759
iamfrdfa <fri.faraji@gmail.com>
2025-07-10 11:15:44 +0200

    added contact form component

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,6 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {ContactformComponent} from './contactform/contactform.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'contact', component: ContactformComponent}
+];
---
+++ portfolio/src/app/contactform/contactform.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+    selector: 'app-contactform',
+    imports: [FormsModule],
+    templateUrl: './contactform.component.html',
+    styleUrl: './contactform.component.scss'
+})
+export class ContactformComponent {
+
+    contactData = {
+        name: "",
+        email: "",
+        message: ""
+    }
+    onSubmit() {
+        console.log(this.contactData);
+    }
+}
