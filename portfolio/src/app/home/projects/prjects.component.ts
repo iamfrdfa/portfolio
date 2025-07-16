d26d2035938961ff30198f3ca404b2dc77119eb4
iamfrdfa <fri.faraji@gmail.com>
2025-07-16 14:15:26 +0200

    added new components: about me, projects, valuation,

---
+++ portfolio/src/app/home/projects/prjects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  imports: [],
+  templateUrl: './prjects.component.html',
+  styleUrl: './prjects.component.scss'
+})
+export class PrjectsComponent {
+
+}
