822328985dc94e80c8cb0406078abb6d59c6056c
iamfrdfa <fri.faraji@gmail.com>
2025-07-18 12:10:34 +0200

    added about me section - initial

--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
 import {RouterOutlet} from '@angular/router';
+import {AboutmeComponent} from './aboutme/aboutme.component';
 
 @Component({
   selector: 'app-home',
     imports: [
         TranslatePipe,
         RouterOutlet,
+        AboutmeComponent,
     ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
