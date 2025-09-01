43e34be6e119acedb982094e3b1688851d5663b7
iamfrdfa <fri.faraji@gmail.com>
2025-09-01 19:20:02 +0200

    changed namespaces for sass after updating sass to V3.0.0

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 
 @Component({
-  selector: 'app-root',
+    selector: 'app-root',
     imports: [RouterOutlet, HeaderComponent, FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio';
+    title = 'portfolio';
 }
--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -1,21 +1,19 @@
-import { Component } from '@angular/core';
-import {TranslatePipe} from '@ngx-translate/core';
+import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {AboutmeComponent} from './aboutme/aboutme.component';
 import {SkillsComponent} from './skills/skills.component';
 import {ProjectsComponent} from './projects/projects.component';
 
 @Component({
-  selector: 'app-home',
+    selector: 'app-home',
     imports: [
-        TranslatePipe,
         RouterOutlet,
         AboutmeComponent,
         SkillsComponent,
         ProjectsComponent,
     ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
