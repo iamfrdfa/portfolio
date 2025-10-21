36a60e29d14fb93e6a37c0265bae8fb85d17aff2
iamfrdfa <fri.faraji@gmail.com>
2025-10-21 09:54:12 +0200

    added scroll down function for back arrow in legal-notice & changed positioning of footer-right links (align left)

--- portfolio/src/app/pages/legal-notice/legal-notice.component.ts
+++ portfolio/src/app/pages/legal-notice/legal-notice.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {TranslatePipe} from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
@@ -10,5 +11,17 @@ import {TranslatePipe} from '@ngx-translate/core';
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
+    constructor(private router: Router) {}
 
+    goHomeToFooter() {
+        this.router.navigate(['/']).then(() => {
+            // kurze Verzögerung, bis die Startseite wirklich geladen ist
+            setTimeout(() => {
+                const footer = document.querySelector('footer');
+                if (footer) {
+                    footer.scrollIntoView({ behavior: 'smooth', block: 'end' });
+                }
+            }, 300);
+        });
+    }
 }
