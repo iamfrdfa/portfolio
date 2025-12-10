e70d8d729e32f80b28343884fa5ee4869e6a208b
iamfrdfa <fri.faraji@gmail.com>
2025-12-10 16:39:45 +0100

    added imprint

--- portfolio/src/app/pages/imprint/imprint.component.ts
+++ portfolio/src/app/pages/imprint/imprint.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  imports: [
+      TranslateModule
+  ],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
-
+    constructor(private router: Router) {}
+    goHomeToFooter() {
+        this.router.navigate(['/']).then(() => {
+            // kurze Verzögerung, bis die Startseite wirklich geladen ist
+            setTimeout(() => {
+                const footer = document.querySelector('footer');
+                if (footer) {
+                    footer.scrollIntoView({ behavior: 'smooth', block: 'end' });
+                }
+            }, 300);
+        });
+    }
 }
